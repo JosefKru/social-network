8da76dc9b5dda2c85f1bd0f7cfbb1d5d79c2f756
JosefKru <josefkaru@gmail.com>
2022-11-15 13:37:45 +0300

    new super logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import style from './Header.module.css'
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/facepalm.png'
 import { NavLink } from 'react-router-dom'
 
 const Header = (props) => {
   return (
     <header className={style.header}>
-      <img src={logo} alt="Лого" />
+      <NavLink to="/">
+        <img src={logo} alt="Лого" />
+      </NavLink>
       <div className={style.login}>
         {props.isAuth ? (
           <div>
